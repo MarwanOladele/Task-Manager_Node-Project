53e96e8afb72f51b38c1653a5e9ca51a0393ea12
MarwanOladele <oladelemarwan@gmail.com>
2023-05-27 17:16:22 +0100

    db setup

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
-const tasks = require('./router/tasks')
+const tasks = require("./router/tasks");
+require("./db/connect");
+const connectDB = require("./db/connect");
 
 // middleware
 app.use(express.json());
 
 // import task routes
-app.use('/api/v1/tasks', tasks)
+app.use("/api/v1/tasks", tasks);
 
 const port = 5000;
-app.listen(port, () => {
-  console.log(`listening on port ${port}...`);
-});
+
+const start = async () => {
+  try {
+    await connectDB();
+
+    app.listen(port, () => {
+      console.log(`listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
