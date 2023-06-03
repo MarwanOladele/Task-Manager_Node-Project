1644b22bf6d0c7073accaaf6e977c213dbb17b57
MarwanOladele <oladelemarwan@gmail.com>
2023-06-03 19:08:45 +0100

    delete task controller implemented

--- controller/tasks.js
+++ controller/tasks.js
@@ -37,8 +37,19 @@ const updateTask = (req, res) => {
   res.send("edit task");
 };
 
-const deleteTask = (req, res) => {
-  res.send("delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ error: `No task with id ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 
 module.exports = {
