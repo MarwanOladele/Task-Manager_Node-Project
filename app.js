b7de86d77fcef5934171f678d1d5697a9ef87ad5
MarwanOladele <oladelemarwan@gmail.com>
2023-06-05 13:18:28 +0100

    project completed

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(notFound);
 // error handler middleware
 app.use(errorHandlerMiddleware);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
