f1b7cb5db5af3f607fb921ea302cf1a6e224ee18
MarwanOladele <oladelemarwan@gmail.com>
2023-06-05 11:23:06 +0100

    erro handler middleware

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const tasks = require("./router/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
-const notFound = require('./middleware/not-found')
-
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 // middleware
 app.use(express.static("./public"));
 app.use(express.json());
@@ -12,8 +12,11 @@ app.use(express.json());
 // import task routes
 app.use("/api/v1/tasks", tasks);
 
-app.use(notFound)
+// not found page middleware
+app.use(notFound);
 
+// error handler middleware
+app.use(errorHandlerMiddleware);
 
 const port = 5000;
 
